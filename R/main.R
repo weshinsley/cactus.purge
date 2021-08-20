6724773d1eeb79182423ac7f72c66a7a7ad1e5f3
Wes Hinsley <wes.hinsley@gmail.com>
2021-08-20 11:03:50 +0100

    Use new keypress branch with_no_echo

--- R/main.R
+++ R/main.R
@@ -12,7 +12,6 @@ library(magrittr)
 close <- function() {
   set_colour(15)
   cursor_on()
-  keypress::restore_term_status()
 }
 
 #' Begin the therapeutic entertainment
@@ -27,36 +26,38 @@ launch <- function() {
   if (!check_windows_ansi()) {
     quit(save = "no", status = 0)
   }
-  keypress::save_term_status()
-  keypress::set_term_echo(FALSE)
 
   on.exit({
     close()
   })
 
-  cat("\033[2J\033[;H")
+  game <- function() {
+    cat("\033[2J\033[;H")
 
-  G <- list(cursor = draw_tv_screen(60, 23),
-            tv_width = 60, tv_height = 23)
+    G <- list(cursor = draw_tv_screen(60, 23),
+              tv_width = 60, tv_height = 23)
 
-  cursor_off()
+    cursor_off()
 
-  if (nrow(audio::audio.drivers()) == 0) {
-    G %<>% show_pic(pkg_file("gfx/nosound.txt"), pattern = "down")
-    k <- keypress::keypress(block = TRUE)
-    G %<>% show_pic(pkg_file("gfx/empty.txt"), pattern = "up")
-  } else if (!check_sound_card()) {
-    G %<>% show_pic(pkg_file("gfx/nosoundcard.txt"), pattern = "down")
-    k <- keypress::keypress(block = TRUE)
-    G %<>% show_pic(pkg_file("gfx/empty.txt"), pattern = "up")
-  }
+    if (nrow(audio::audio.drivers()) == 0) {
+      G %<>% show_pic(pkg_file("gfx/nosound.txt"), pattern = "down")
+      k <- keypress::keypress(block = TRUE)
+      G %<>% show_pic(pkg_file("gfx/empty.txt"), pattern = "up")
+    } else if (!check_sound_card()) {
+      G %<>% show_pic(pkg_file("gfx/nosoundcard.txt"), pattern = "down")
+      k <- keypress::keypress(block = TRUE)
+      G %<>% show_pic(pkg_file("gfx/empty.txt"), pattern = "up")
+    }
 
-  while (TRUE) {
-    G %<>% main_title()
-    if (G$main_menu_result == "CACTZ") {
-      G %<>% cactz_title()
-    } else {
-      break
+    while (TRUE) {
+      G %<>% main_title()
+      if (G$main_menu_result == "CACTZ") {
+        G %<>% cactz_title()
+      } else {
+        break
+      }
     }
   }
+
+  keypress::with_no_echo(game())
 }
